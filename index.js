251829cf999f9a36527ab1efd83657418c3ad2c1
sascha.bast <Qicklink1>
2018-12-15 10:12:44 +0100

    SLUDGE: Banner erstellen -> https://metroplex.dgm-it.de/#/5621

--- index.js
+++ index.js
@@ -11,42 +11,12 @@ const fs = require('fs')
 const babel = require('babel-core')
 const mkdirp = require('mkdirp')
 const {minify: htmlMinify} = require('html-minifier')
-const o = require('yargs')
-    .usage('Usage: $0 -s <src-dir> -d <dest-dir>')
-    .option('s', {
-        alias: 'src-dir', type: 'string', demand: true,
-        describe: 'Source directory',
-    })
-    .option('d', {
-        alias: 'dest-dir', type: 'string', demand: true,
-        describe: 'Destination directory',
-    })
-    .option('D', {
-        alias: 'copy-files', type: 'boolean',
-        describe: 'Copy non-compilable files',
-    })
-    .option('w', {
-        alias: 'watch', type: 'boolean',
-        describe: 'Watch mode',
-    })
-    .option('m', {
-        alias: 'minify', type: 'boolean',
-        describe: 'Minify HTML, CSS nad JS',
-    })
-    .option('q', {
-        alias: 'quiet', type: 'boolean',
-        describe: 'Disable logging',
-    })
-    .help('h')
-    .alias('h', 'help')
-    .strict()
-    .argv
 
 const printError = err => console.error(err.message)
 
-const transformHtml = co.wrap(function* (src, dest, {minify}) {
+const transformHtml = function (src, dest, {minify}) {
     if (!o.q) console.log(`${src} -> ${dest}`)
-    const dom = yield p(fs.readFile)(src, 'utf8').then(cheerio.load)
+    const dom =  p(fs.readFile)(src, 'utf8').then(cheerio.load)
     dom('script:not([src])').each((i, el) => {
         const input = dom(el).text()
         const [firstLine] = input.match(/^.*\S.*$/m) || [';']
@@ -60,41 +30,43 @@ const transformHtml = co.wrap(function* (src, dest, {minify}) {
         else output = '\n' + output.replace(/^\n/, '').replace(/^/gm, indent) + end
         dom(el).text(output)
     })
-    yield p(mkdirp)(path.dirname(dest)).catch(printError)
+     p(mkdirp)(path.dirname(dest)).catch(printError)
     let output = dom.html()
     if (minify) output = htmlMinify(output, {
         collapseInlineTagWhitespace: true, collapseWhitespace: true,
         minifyCSS: true,
     })
-    yield p(fs.writeFile)(dest, output)
-})
+     p(fs.writeFile)(dest, output)
+}
 
-const transformJs = co.wrap(function* (src, dest, {minify}) {
+const transformJs = function (src, dest, {minify}) {
     if (!o.q) console.log(`${src} -> ${dest}`)
-    const output = yield p(babel.transformFile)(src, {
+    const output =  p(babel.transformFile)(src, {
         compact: !!minify, comments: !minify, minified: !!minify,
     }).then(out => out.code)
-    yield p(mkdirp)(path.dirname(dest)).catch(printError)
-    yield p(fs.writeFile)(dest, output)
-})
+     p(mkdirp)(path.dirname(dest)).catch(printError)
+     p(fs.writeFile)(dest, output)
+}
 
-const copy = co.wrap(function* (src, dest) {
+const copy = function (src, dest) {
     if (!o.q) console.log(`${src} -> ${dest}`)
-    yield p(mkdirp)(path.dirname(dest)).catch(printError)
-    yield new Promise((resolve, reject) =>
+     p(mkdirp)(path.dirname(dest)).catch(printError)
+     new Promise((resolve, reject) =>
         fs.createReadStream(src)
             .pipe(fs.createWriteStream(dest))
             .on('finish', resolve)
             .on('error', reject)
     )
-})
+}
 
-chokidar.watch('.', {cwd: o.s, persistent: o.w})
-    .on('all', (ev, f) => {
-        if (!['add', 'change'].includes(ev)) return
-        const [src, dest] = [path.join(o.s, f), path.join(o.d, f)]
-        const opt = {minify: !!o.m}
-        if (f.match(/\.html?$/)) transformHtml(src, dest, opt).catch(printError)
-        else if (f.match(/\.jsx?$/)) transformJs(src, dest, opt).catch(printError)
+const transform = function (src, dest, minify) {
+    let srcFs = fs.readdirSync(src);
+    const opt = {minify: minify}
+
+    for (let file of srcFs) {
+        if (file.match(/\.html?$/)) transformHtml(src, dest, opt).catch(printError)
+        else if (file.match(/\.jsx?$/)) transformJs(src, dest, opt).catch(printError)
         else if (o.D) copy(src, dest, opt).catch(printError)
-    })
+    }
+
+}
