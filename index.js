5bf15b203692269bcf1fb010ca6cad43f63d302e
sascha.bast <Qicklink1>
2018-12-15 10:23:34 +0100

    SLUDGE: Banner erstellen -> https://metroplex.dgm-it.de/#/5621

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const {minify: htmlMinify} = require('html-minifier')
 const printError = err => console.error(err.message)
 
 const transformHtml = function (src, dest, {minify}) {
-    if (!o.q) console.log(`${src} -> ${dest}`)
-    const dom =  p(fs.readFile)(src, 'utf8').then(cheerio.load)
+    console.log(`${src} -> ${dest}`)
+    const dom = p(fs.readFile)(src, 'utf8').then(cheerio.load)
     dom('script:not([src])').each((i, el) => {
         const input = dom(el).text()
         const [firstLine] = input.match(/^.*\S.*$/m) || [';']
@@ -28,29 +28,29 @@ const transformHtml = function (src, dest, {minify}) {
         else output = '\n' + output.replace(/^\n/, '').replace(/^/gm, indent) + end
         dom(el).text(output)
     })
-     p(mkdirp)(path.dirname(dest)).catch(printError)
+    p(mkdirp)(path.dirname(dest)).catch(printError)
     let output = dom.html()
     if (minify) output = htmlMinify(output, {
         collapseInlineTagWhitespace: true, collapseWhitespace: true,
         minifyCSS: true,
     })
-     p(fs.writeFile)(dest, output)
+    p(fs.writeFile)(dest, output)
 }
 
 
 const transformJs = function (src, dest, {minify}) {
-    if (!o.q) console.log(`${src} -> ${dest}`)
-    const output =  p(babel.transformFile)(src, {
+    console.log(`${src} -> ${dest}`)
+    const output = p(babel.transformFile)(src, {
         compact: !!minify, comments: !minify, minified: !!minify,
     }).then(out => out.code)
-     p(mkdirp)(path.dirname(dest)).catch(printError)
-     p(fs.writeFile)(dest, output)
+    p(mkdirp)(path.dirname(dest)).catch(printError)
+    p(fs.writeFile)(dest, output)
 }
 
 const copy = function (src, dest) {
-    if (!o.q) console.log(`${src} -> ${dest}`)
-     p(mkdirp)(path.dirname(dest)).catch(printError)
-     new Promise((resolve, reject) =>
+     console.log(`${src} -> ${dest}`)
+    p(mkdirp)(path.dirname(dest)).catch(printError)
+    new Promise((resolve, reject) =>
         fs.createReadStream(src)
             .pipe(fs.createWriteStream(dest))
             .on('finish', resolve)
@@ -61,11 +61,10 @@ const copy = function (src, dest) {
 const transform = function (src, dest, minify) {
     let srcFs = fs.readdirSync(src);
     const opt = {minify: minify}
-
     for (let file of srcFs) {
         if (file.match(/\.html?$/)) transformHtml(src, dest, opt).catch(printError)
         else if (file.match(/\.jsx?$/)) transformJs(src, dest, opt).catch(printError)
-        else if (o.D) copy(src, dest, opt).catch(printError)
+        copy(src, dest, opt).catch(printError)
     }
 
 }
